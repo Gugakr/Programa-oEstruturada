ba4058f2a182553397d43e7f586e2e2e94f77a1b
Gugakr <2025320934@aluno.canoas.ifrs.edu.br>
2026-03-10 16:59:13 -0300

    commit aula3.c

---
+++ aula3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    float num1,num2,result,result2;
+    printf("Digite dois numero real: ");
+    scanf("%f%f",&num1,&num2);
+    //result = fmod(num1,num2);-->calcula resto de numeros reais | floor arredonda pra baixo e ceil pra cima
+    //result = pow(num1,num2);-->potencia de 2 numeros
+    //result = sqrt(num1);-->raiz quadrada
+    //result2 = cbrt(num2);-->raiz cubica
+    //printf("Resultados:\n %2.f%.2f",result,result2);
+    //printf("Resultado: %f",M_PI);-->escrever o numero PI
+    return 0;
+}
